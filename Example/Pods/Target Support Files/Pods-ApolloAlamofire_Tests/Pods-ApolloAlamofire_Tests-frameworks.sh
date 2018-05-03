2dca81d192e60afcbb7609af4b1439f74f2e4875
Max Desiatov <max.desiatov@gmail.com>
2018-05-03 10:47:17 +0100

    Added simple test, updated example dependencies

--- Example/Pods/Target Support Files/Pods-ApolloAlamofire_Tests/Pods-ApolloAlamofire_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ApolloAlamofire_Tests/Pods-ApolloAlamofire_Tests-frameworks.sh
@@ -143,9 +143,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Apollo/Apollo.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ApolloAlamofire/ApolloAlamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Apollo/Apollo.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ApolloAlamofire/ApolloAlamofire.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
