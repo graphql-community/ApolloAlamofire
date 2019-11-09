e9cd72ef027b84aa0fa241f2ee519aa0b14f8078
Max Desiatov <max@desiatov.com>
2019-11-09 20:50:08 +0000

    Fix multi-line invocation in build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e 
 set -o pipefail
 
-xcodebuild test -enableCodeCoverage YES 
-    -workspace Example/ApolloAlamofire.xcworkspace -scheme Example
+xcodebuild test -enableCodeCoverage YES \
+    -workspace Example/ApolloAlamofire.xcworkspace -scheme Example \
     -sdk iphonesimulator -destination "$TEST_DEVICE" | xcpretty
 pod repo update
 pod lib lint --allow-warnings
