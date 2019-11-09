6bbbf8b0d77e42429aeaad846c22e40472d9d1e1
Max Desiatov <max@desiatov.com>
2019-11-09 21:05:10 +0000

    Avoid running `pod repo update` repeatedly

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ set -o pipefail
 xcodebuild test -enableCodeCoverage YES \
     -workspace Example/ApolloAlamofire.xcworkspace -scheme Example \
     -sdk iphonesimulator -destination "$TEST_DEVICE" | xcpretty
-pod repo update
 pod lib lint --allow-warnings
