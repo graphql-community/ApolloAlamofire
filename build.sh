356cfb02ef46ecdb3badca4250b959e9541a182f
Max Desiatov <max@desiatov.com>
2019-11-09 20:35:20 +0000

    Move build steps to a separate script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+set -e 
+set -o pipefail
+
+xcodebuild test -enableCodeCoverage YES 
+    -workspace Example/ApolloAlamofire.xcworkspace -scheme Example
+    -sdk iphonesimulator -destination "$TEST_DEVICE" | xcpretty
+pod repo update
+pod lib lint --allow-warnings
